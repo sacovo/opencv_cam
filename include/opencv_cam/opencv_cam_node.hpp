90e00d37780ca4433c284d9925fcdd2eaac8131e
Sandro Covo <sandro@sandrocovo.ch>
2024-03-13 16:41:24 +0100

    Use image_transport for image transmission

--- include/opencv_cam/opencv_cam_node.hpp
+++ include/opencv_cam/opencv_cam_node.hpp
@@ -1,12 +1,13 @@
 #ifndef OPENCV_CAM_HPP
 #define OPENCV_CAM_HPP
 
-
 #include "opencv2/highgui/highgui.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/camera_info.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "sensor_msgs/msg/compressed_image.hpp"
+
+#include "image_transport/image_transport.hpp"
 
 #include "opencv_cam/camera_context.hpp"
 
@@ -26,26 +27,20 @@ namespace opencv_cam
     int publish_fps_;
     rclcpp::Time next_stamp_;
 
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_, image_pub2_;
-    rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr cimage_pub_, cimage_pub2_;
-
+    image_transport::Publisher image_pub_, image_pub2_;
     rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_pub_;
 
   public:
-
     explicit OpencvCamNode(const rclcpp::NodeOptions &options);
 
     ~OpencvCamNode() override;
 
   private:
-
     void validate_parameters();
 
     void loop();
 
-    
-    void create_and_publish_image(rclcpp::Time &stamp, cv::Mat &frame, std::string frame_id, rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub);
-    void create_and_publish_cimage(std::string compression, rclcpp::Time &stamp, cv::Mat &frame, std::string frame_id, rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr pub);
+    void create_and_publish_image(rclcpp::Time &stamp, cv::Mat &frame, std::string frame_id, image_transport::Publisher pub);
 
     sensor_msgs::msg::Image::UniquePtr create_image_msg(rclcpp::Time &stamp, cv::Mat &frame, std::string frame_id);
     sensor_msgs::msg::CompressedImage::UniquePtr create_cimage_msg(rclcpp::Time &stamp, cv::Mat &frame, std::string frame_id, std::string compression);
@@ -53,4 +48,4 @@ namespace opencv_cam
 
 } // namespace opencv_cam
 
-#endif //OPENCV_CAM_HPP
+#endif // OPENCV_CAM_HPP
